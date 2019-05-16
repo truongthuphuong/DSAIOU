488641e7d56ce6a9e3b077ff4b8534ff212ea8e8
Nhung2201 <49773215+Nhung2201@users.noreply.github.com>
2019-05-16 16:59:35 +0700

    Delete Cau7.py

--- Cau7.py
+++
@@ -1,9 +0,0 @@
-print (abs.__doc__)
-print (int.__doc__)
-print (input.__doc__)
-def square(num):
- '''Trả lại giá trị bình phương của số được nhập vào.
- Số nhập vào phải là số nguyên.
- '''
- return num ** 2
-print (square.__doc__)
